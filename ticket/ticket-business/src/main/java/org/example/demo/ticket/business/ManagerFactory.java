6294e490598d4187ea9a18a6c87f90eebb8c5953
Kikoolami <christophe.gomes.pub@free.fr>
2021-02-14 22:46:41 +0100

    injection de dependance par design pattern factory
    injection de dependance par design pattern factory

---
+++ ticket/ticket-business/src/main/java/org/example/demo/ticket/business/ManagerFactory.java
@@ -0,0 +1,28 @@
+package org.example.demo.ticket.business;
+
+import org.example.demo.ticket.business.manager.ProjetManager;
+import org.example.demo.ticket.business.manager.TicketManager;
+
+public class ManagerFactory {
+
+    private static final ManagerFactory INSTANCE = new ManagerFactory();
+
+    public ManagerFactory() {
+        super();
+    }
+
+    public static ManagerFactory getInstance() {
+        return ManagerFactory.INSTANCE;
+    }
+
+    public ProjetManager getProjetManager() {
+        return new ProjetManager();
+    }
+
+    public TicketManager getTicketManager() {
+        return new TicketManager();
+    }
+
+
+
+}
